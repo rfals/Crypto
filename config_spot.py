80f8b418e6de0a857de52f5739bbf4156bbbaaf7
rfals <91599078+rfals@users.noreply.github.com>
2022-05-14 17:22:06 +0300

    Update and rename config_spot.py to Triangle_arb/config_spot.py

--- config_spot.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[3]:
-
-
-API = 'm7jNDBUawJqRlDapzSaYLlb5FxYO8ZBbmS9t1uexkTfZHyhcM19noOxFIKCk8rVy'
-API_secret = 'yKxu1VfwTQg7jzci9GqkByF5DW7G86BtvLsquQv9RPJmmYGUku6YtG4YhSUZc5WE'
-
